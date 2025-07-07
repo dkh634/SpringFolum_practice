ccd616c18f377de26fcabaf5f3b9027dd40a1fde
dkh634 <karinto634@outlook.jp>
2025-07-07 20:58:18 +0900

    CI修正

--- src/main/java/jp/dkh634/springforum/controller/ForumController.java
+++ src/main/java/jp/dkh634/springforum/controller/ForumController.java
@@ -1,15 +1,18 @@
 package jp.dkh634.springforum.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import jp.dkh634.springforum.service.ForumService;
 
-@RequestMapping("/api/forum")
-@RestController 
+@Controller
 public class ForumController {
 	@Autowired
     private ForumService forumService;
 
+	@GetMapping("/api/home")
+	public String displayHome() {
+		return "home";
+	}
 }
