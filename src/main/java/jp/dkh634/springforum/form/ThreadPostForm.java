b712bff61cbc38071b9dbb13e455696db9fb811c
dkh634 <karinto634@outlook.jp>
2025-07-26 01:47:35 +0900

    新規スレッドの作成

---
+++ src/main/java/jp/dkh634/springforum/form/ThreadPostForm.java
@@ -0,0 +1,8 @@
+package jp.dkh634.springforum.form;
+
+import lombok.Data;
+
+@Data
+public class ThreadPostForm {
+	private String title;
+}
--- src/main/java/jp/dkh634/springforum/repository/ThreadRepository.java
+++ src/main/java/jp/dkh634/springforum/repository/ThreadRepository.java
@@ -3,8 +3,11 @@ package jp.dkh634.springforum.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.dkh634.springforum.entity.ForumThread;
 
@@ -31,4 +34,25 @@ public interface ThreadRepository extends JpaRepository<ForumThread, Long> {
      */
     @Query(value = "SELECT title FROM thread WHERE thread_id = :threadId", nativeQuery = true)
     String findTitle(Long threadId);
+    
+    /**
+     * 論理削除されていない最大のThreadIdを取得する。
+     *
+     * @param threadId スレッドのID
+     * @return 該当スレッドのタイトル文字列
+     */
+    @Query(value = "SELECT MAX(thread_id) FROM thread", nativeQuery = true)
+    Integer findMaxThreadId();
+    
+    @Modifying
+    @Transactional
+    @Query(value = """
+        INSERT INTO thread (thread_id, title, created_at)
+        VALUES (:threadId, :title, CURRENT_TIMESTAMP)
+    """, nativeQuery = true)
+    void saveForum(
+        @Param("threadId") Long threadId,
+        @Param("title") String title
+    );
+
 }
