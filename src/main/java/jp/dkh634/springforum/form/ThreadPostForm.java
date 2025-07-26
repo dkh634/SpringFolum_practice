72bc1e4ff0aebde794aa5c415496e445b93f03e3
dkh634 <karinto634@outlook.jp>
2025-07-26 21:10:56 +0900

    微修正
    @notblank に修正
    ValidationMessages.propertiesの微修正

--- src/main/java/jp/dkh634/springforum/form/ThreadPostForm.java
+++ src/main/java/jp/dkh634/springforum/form/ThreadPostForm.java
@@ -1,12 +1,13 @@
 package jp.dkh634.springforum.form;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ThreadPostForm {
 	
-//	@NotBlank(message = "{threadPostForm.title.notNull}")
-	@Size(min = 1, max = 20, message = "{threadPostForm.title.size}")
+	@NotBlank(message = "{threadPostForm.title.notNull}")
+	@Size(max = 20, message = "{threadPostForm.title.size}")
 	private String title;
 }
